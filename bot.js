2bb5a12f491245ace5d1fb2350d5ea7ef9ba7b62
menjazhdut <129472273+menjazhdut@users.noreply.github.com>
2023-03-31 17:28:34 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,14 @@
 javascript
-const Telegraf = require('telegraf');
-const bot = new Telegraf('6008668981:AAE-GMnCqmITBohOVJ2o3AN8hzYjvPqnjTA');
+const TelegramBot = require('node-telegram-bot-api');
 
-bot.start((ctx) => {
-    ctx.reply('Привет! Я бот на Telegram!');
+// Токен вашего бота, полученный у BotFather
+const token = '6008668981:AAE-GMnCqmITBohOVJ2o3AN8hzYjvPqnjTA';
+
+const bot = new TelegramBot(token, {polling: true});
+
+// Обработчик команды /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Привет! Я Telegram-бот.');
 });
-
-bot.help((ctx) => {
-    ctx.reply('Отправьте мне сообщение и я отвечу!');
-});
-
-bot.hears('привет', (ctx) => {
-    ctx.reply('Привет!');
-});
-
-bot.on('text', (ctx) => {
-    ctx.reply(`Вы прислали текст: ${ctx.message.text}`);
-});
-
-bot.launch();
-console.log('Бот успешно запущен');
