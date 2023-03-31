e97df9621815ca5bb937cbeea4f0b14487e3cda0
menjazhdut <129472273+menjazhdut@users.noreply.github.com>
2023-03-31 17:54:26 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
+
 javascript
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
 
-// Токен вашего бота, полученный у BotFather
-const token = '6008668981:AAE-GMnCqmITBohOVJ2o3AN8hzYjvPqnjTA';
-
+const token = '6008668981:AAE-GMnCqmITBohOVJ2o3AN8hzYjvPqnjTA'; // здесь нужно указать токен вашего бота
 const bot = new TelegramBot(token, {polling: true});
 
-// Обработчик команды /start
-bot.onText(/\/start/, (msg) => {
+bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
+  const messageText = msg.text;
 
-  bot.sendMessage(chatId, 'Привет! Я Telegram-бот.');
+  if (messageText === '/quote') {
+    try {
+      const response = await axios.get('https://api.kanye.rest/');
+      const quote = response.data.quote;
+      bot.sendMessage(chatId, quote);
+    } catch (error) {
+      console.log(error);
+      bot.sendMessage(chatId, 'Произошла ошибка при получении цитаты :(');
+    }
+  }
 });
